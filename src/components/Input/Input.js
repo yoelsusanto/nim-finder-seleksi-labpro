5cb410687b943230aa2beda86add09758718c300
Yoel Susanto <yoelsusantopro@yahoo.com>
2019-06-13 22:45:35 +0700

    Add input component

---
+++ src/components/Input/Input.js
@@ -0,0 +1,28 @@
+import React, { Component } from "react";
+
+export class Input extends Component {
+  state = { input: "" };
+
+  onChange = event => {
+    event.persist();
+    this.setState({ input: event.target.value }, () => {
+      // console.log(event.target);
+      this.props.onChange(event);
+    });
+  };
+
+  render() {
+    return (
+      <div className="field">
+        <label>{this.props.label}</label>
+        <input
+          type={this.props.className === "password" ? "password" : "text"}
+          placeholder={this.props.placeholder}
+          value={this.state.input}
+          onChange={this.onChange}
+          className={this.props.className}
+        />
+      </div>
+    );
+  }
+}
