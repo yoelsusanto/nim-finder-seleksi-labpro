4387b1be0103c4a58ff5e6ecc98ef44ba95f635e
Yoel Susanto <yoelsusantopro@yahoo.com>
2019-06-13 22:37:14 +0700

    Move components

--- src/components/ResultCard.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const ResultCard = () => {
-  return <div>ResultCard</div>;
-};
-
-export default ResultCard;
--- src/components/SearchBar.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-class SearchBar extends React.Component {
-  render() {
-    <div>SearchBar</div>;
-  }
-}
-
-export default SearchBar;
