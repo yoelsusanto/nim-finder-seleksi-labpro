da70ad55656b3d2fe7f3258e50356dbbdc61d460
Yoel Susanto <yoelsusantopro@yahoo.com>
2019-06-17 23:52:47 +0700

    Create routing and connect redux action creators

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,49 @@
-import React from "react";
+import React, { Component } from "react";
+import { Route, Switch, Redirect } from "react-router-dom";
+import { Layout } from "../scenes/Layout";
+import { Login } from "../scenes/Layout/Login";
+import { NIMFinder } from "../scenes/Layout/NIMFinder";
+import { connect } from "react-redux";
 
-class App extends React.Component {
+import { tryLoginAtStart, logoutOnTokenExpire } from "../store/actions";
+
+class App extends Component {
+  componentDidMount() {
+    this.props.tryLoginAtStart();
+  }
+
   render() {
-    <div>App</div>;
+    return (
+      <div className="ui container" style={this.style}>
+        <Layout>
+          <Switch>
+            <Route path="/login" component={Login} />
+            {!this.props.isLoggedIn ? <Redirect to="/login" /> : null}
+            {this.props.isLoggedIn ? (
+              <Route path="/" component={NIMFinder} />
+            ) : null}
+          </Switch>
+        </Layout>
+      </div>
+    );
   }
 }
 
-export default App;
+const mapStateToProps = state => {
+  return {
+    isLoggedIn: state.auth.userToken != null,
+    logoutTimer: state.auth.logoutTimer
+  };
+};
+
+const mapDispatchToProps = dispatch => {
+  return {
+    tryLoginAtStart: () => dispatch(tryLoginAtStart()),
+    logoutOnTokenExpire: duration => dispatch(logoutOnTokenExpire(duration))
+  };
+};
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(App);
