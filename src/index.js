572144d372c5d4f48936f4a2f502e4a31651062c
Yoel Susanto <yoelsusantopro@yahoo.com>
2019-06-17 23:51:38 +0700

    Connect index.js to redux and react-router

--- src/index.js
+++ src/index.js
@@ -1,6 +1,29 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import thunk from "redux-thunk";
+import { createStore, applyMiddleware, combineReducers, compose } from "redux";
 
 import App from "./components/App";
+import { authReducer, searchReducer } from "./store/reducers";
 
-ReactDOM.render(<App />, document.querySelector("#root"));
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const combinedReducers = combineReducers({
+  auth: authReducer,
+  search: searchReducer
+});
+const store = createStore(
+  combinedReducers,
+  composeEnhancers(applyMiddleware(thunk))
+);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>,
+  document.querySelector("#root")
+);
